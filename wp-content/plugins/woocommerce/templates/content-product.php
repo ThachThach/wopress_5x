2d7b31a195431b728088f36e83f488603a79ccd6
ThachThach <thachhaise@gmail.com>
2021-05-24 16:22:57 +0700

    "them so luong hang"

--- wp-content/plugins/woocommerce/templates/content-product.php
+++ wp-content/plugins/woocommerce/templates/content-product.php
@@ -47,6 +47,7 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 	 * @hooked woocommerce_template_loop_product_title - 10
 	 */
 	do_action( 'woocommerce_shop_loop_item_title' );
+	echo "Sản phẩm còn trong kho: " .$product->get_stock_quantity();
 
 	/**
 	 * Hook: woocommerce_after_shop_loop_item_title.
@@ -55,6 +56,8 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 	 * @hooked woocommerce_template_loop_price - 10
 	 */
 	do_action( 'woocommerce_after_shop_loop_item_title' );
+
+	
 
 	/**
 	 * Hook: woocommerce_after_shop_loop_item.
